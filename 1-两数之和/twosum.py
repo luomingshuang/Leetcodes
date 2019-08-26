994d53890f0ca77431e309afd903839cf3894410
Mingshuang Luo <37799481+luomingshuang@users.noreply.github.com>
2019-08-26 10:07:32 +0800

    Update twosum.py

--- 1-两数之和/twosum.py
+++ 1-两数之和/twosum.py
@@ -11,3 +11,15 @@ class Solution:
         for sum in sums:
             if sum[0] == target:
                 return sum[1],sum[2]
+###法二：差值搜索
+class Solution:
+    def twoSum(self, nums, target):
+        N = len(nums)
+        sums = []
+        for idx in range(N):
+            res = target - nums[idx]
+            print(nums[idx], res)
+            print(nums[idx+1:])
+            if res in nums[idx+1:]:
+                #print(nums[idx+1:])
+                return idx, nums[idx+1:].index(res)+idx+1
